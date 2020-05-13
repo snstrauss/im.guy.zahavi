db7d7ae66e1cc18386b105e0bd767af2255b7af0
snstrauss <zahavi.guy.85@gmail.com>
2020-05-13 13:40:51 +0300

    clean up logs and breakpoints

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,10 @@
 	import { Router } from "@sveltech/routify";
 	import { routes } from "@sveltech/routify/tmp/routes";
 
-	console.log('sessionStorage: ');
-	console.log(sessionStorage);
-	debugger;
-
 	(function redirectIfNeeded(){
-		console.log('redirect if needed!');
-		debugger;
-
 		const redirect = sessionStorage.redirect;
 		delete sessionStorage.redirect;
 		if (redirect && redirect !== location.href) {
-			console.log('history replace state!');
 			history.replaceState(null, null, redirect);
 		}
 	})()
