f80afddf73d502b24caa3d884c06ce0eb6398d90
snstrauss <zahavi.guy.85@gmail.com>
2020-05-19 16:39:59 +0300

    <Header> component now sets type of <Nav> menu according to screen width (bound to window.innerWidth)

--- src/components/header/header.svelte
+++ src/components/header/header.svelte
@@ -1,7 +1,13 @@
 <script>
     import Nav from './nav.svelte';
     import Socials from './socials.svelte';
+
+    let viewWidth;
+    let isBigScreen;
+    $: isBigScreen = (viewWidth > 600);
 </script>
+
+<svelte:window bind:innerWidth={viewWidth} />
 
 <style lang="scss">
     header {
@@ -9,10 +15,17 @@
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
+
+        height: var(--header-height);
+
+        &.singleElement {
+            justify-content: flex-end;
+        }
     }
 </style>
 
-<header class="header">
-    <Nav />
+<header class="header" class:singleElement={!isBigScreen}>
+    <Nav onSide={!isBigScreen} />
     <Socials />
 </header>
+
