743311c18d99ee7377b42e6cc64c94843fe92dde
snstrauss <zahavi.guy.85@gmail.com>
2020-05-19 16:50:49 +0300

    created <NavLinks> component. active link, as well as hover upon link, is now painted black.

---
+++ src/components/header/nav-links.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { layout, goto, isActive } from '@sveltech/routify';
+
+    const titleTranslations = {
+        "im.guy.zahavi": "Guy"
+    };
+
+    const navLinks = $layout.children.map(({ title, path }) => ({
+        title: titleTranslations[title] ? titleTranslations[title] : title,
+        path
+    }));
+</script>
+
+<style lang="scss">
+    .links-container {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        position: absolute;
+        top: var(--header-height);
+
+        .nav-link {
+            margin: 0;
+            background: none;
+            border: none;
+            outline: none;
+
+            color: rgba(0, 0, 0, 0.5);
+            transition: color 0.2s;
+            &:hover, &.active {
+                color: rgba(0, 0, 0, 1);
+            }
+        }
+    }
+</style>
+
+<div class="links-container">
+    {#each navLinks as { title, path}}
+        <button class="nav-link" class:active={$isActive(path)} on:click={$goto(path)}>&lt;{title} /&gt;</button>
+    {/each}
+</div>
