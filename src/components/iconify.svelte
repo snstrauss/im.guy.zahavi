5ebe8bf0dc76880efd222babccf02f7c71cfb51b
snstrauss <zahavi.guy.85@gmail.com>
2020-06-04 13:01:43 +0300

    created <Iconify> component to hold the Iconify library logic

---
+++ src/components/iconify.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Iconify from '@iconify/iconify';
+
+    export let icon = 'logos:svg';
+    export let inline = true;
+    export let style = {};
+    export let height = null;
+
+    const styleString = typeof style === 'string' ? style : parseStyle(style);
+
+    function parseStyle(styleObj){
+        return Object.entries(styleObj).map(([k, v]) => `${toKebabCase(k)}:${v}`).join(';');
+    }
+
+    function toKebabCase(str){
+        return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
+    }
+
+    setTimeout(() => {
+        const self = document.querySelectorAll(`[data-icon="${icon}"]`)[0];
+        self.style.setProperty('vertical-align', '-0.2em');
+    }, 50);
+</script>
+
+<iconify-icon data-icon={icon}
+                data-inline={inline}
+                data-align="bottom"
+                data-height={height}
+                style={styleString}>
+</iconify-icon>
