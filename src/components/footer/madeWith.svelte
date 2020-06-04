3a7dc86715ef020dd1498d65d5110fce8f403ade
snstrauss <zahavi.guy.85@gmail.com>
2020-06-04 13:09:19 +0300

    created <MadeWith> component with link to svelte website

---
+++ src/components/footer/madeWith.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Iconify from "./../iconify.svelte";
+</script>
+
+<style lang="scss">
+    .container {
+        opacity: 0.8;
+    }
+</style>
+
+<span class="container">
+    <Iconify icon="entypo-code"/>
+    with
+    <Iconify style={{color: '#e71837' }} icon="entypo-heart"/>
+    using
+    <a href="https://svelte.dev/" target="_"><Iconify style={{color: '#FF3E00'}} icon="simple-icons:svelte"/></a>
+</span>
