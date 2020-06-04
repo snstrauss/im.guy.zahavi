ede4d58958d71fee6256a0ef29db7a1f4be2edaa
snstrauss <zahavi.guy.85@gmail.com>
2020-06-04 13:08:53 +0300

    created <EmailMe> component that shows an email and sends to it when clicked.

---
+++ src/components/footer/emailMe.svelte
@@ -0,0 +1,46 @@
+<script>
+    import Iconify from '../iconify.svelte';
+
+    export let email;
+    export let isBigScreen = false;
+</script>
+
+<style lang="scss">
+    a {
+        --glow-shadow: 0 0 1px white;
+        --transition-props: 0.2s linear;
+
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-evenly;
+
+        text-transform: uppercase;
+        color: white;
+        letter-spacing: min(0.5vw, 0.3em);
+
+        transition: text-shadow var(--transition-props);
+        :global(svg) {
+            transition: filter var(--transition-props);
+        }
+        &:hover {
+            text-decoration-line: none;
+            text-shadow: var(--glow-shadow);
+
+            :global(svg) {
+                filter: drop-shadow(var(--glow-shadow));
+            }
+        }
+
+        &.isBigScreen {
+            width: 45%;
+        }
+    }
+
+    :global(.email-me-icon){
+        color: white;
+    }
+</style>
+
+<a href={`mailto:${email}`} class:isBigScreen><Iconify height="20" icon="mdi-email-outline" />{email}</a>
